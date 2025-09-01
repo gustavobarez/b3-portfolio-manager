d7a925eddd5d472b82a85b498bb8701c78fc218a
gustavobarez <gbarez44@gmail.com>
2025-09-01 14:54:17 -0300

    chore: add initial Spring files

---
+++ src/main/java/br/com/gustavobarez/Itau/Tech/Challenge/ItauTechChallengeApplication.java
@@ -0,0 +1,13 @@
+package br.com.gustavobarez.Itau.Tech.Challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItauTechChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItauTechChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gustavobarez/Itau/Tech/Challenge/ItauTechChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gustavobarez.Itau.Tech.Challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItauTechChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
