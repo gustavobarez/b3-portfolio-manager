5da154e1a8a652967f98de683d0b085c552134eb
gustavobarez <gbarez44@gmail.com>
2025-09-02 19:57:15 -0300

    feat: add index, optimized query and trigger

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,3 +1,7 @@
+CREATE DATABASE itau_tech_challenge;
+
+USE itau_tech_challenge;
+
 CREATE TABLE
     users (
         id BIGINT AUTO_INCREMENT PRIMARY KEY,
@@ -22,7 +26,7 @@ CREATE TABLE
         unit_price DECIMAL(19, 4) NOT NULL,
         operation_type VARCHAR(6) NOT NULL,
         brokerage_fee DECIMAL(19, 4) NOT NULL,
-        timestamp TIMESTAMP NOT NULL,
+        date_time TIMESTAMP NOT NULL,
         FOREIGN KEY (user_id) REFERENCES users (id),
         FOREIGN KEY (asset_id) REFERENCES assets (id)
     );
@@ -32,7 +36,7 @@ CREATE TABLE
         id BIGINT AUTO_INCREMENT PRIMARY KEY,
         asset_id BIGINT NOT NULL,
         unit_price DECIMAL(19, 4) NOT NULL,
-        timestamp TIMESTAMP NOT NULL,
+        date_time TIMESTAMP NOT NULL,
         FOREIGN KEY (asset_id) REFERENCES assets (id)
     );
 
@@ -43,9 +47,36 @@ CREATE TABLE
         asset_id BIGINT NOT NULL,
         quantity INT NOT NULL,
         average_price DECIMAL(19, 4) NOT NULL,
-        p_and_l DECIMAL(19, 4) NOT NULL,
+        profit_and_loss DECIMAL(19, 4) NOT NULL,
         UNIQUE (user_id, asset_id),
         FOREIGN KEY (user_id) REFERENCES users (id),
         FOREIGN KEY (asset_id) REFERENCES assets (id)
     );
 
+CREATE INDEX idx_operations_user_asset_time ON operations (user_id, asset_id, date_time);
+
+SELECT
+    *
+FROM
+    operations
+WHERE
+    user_id = 1
+    AND asset_id = 1
+    AND date_time >= DATE_SUB (NOW (), INTERVAL 30 DAY)
+ORDER BY
+    date_time DESC;
+
+DELIMITER $$
+CREATE TRIGGER tg_update_pnl_position
+AFTER INSERT ON quotations
+FOR EACH ROW
+BEGIN
+    UPDATE
+        positions
+    SET
+        profit_and_loss = (NEW.unit_price - average_price) * quantity
+    WHERE
+        asset_id = NEW.asset_id;
+END$$
+
+DELIMITER ;
